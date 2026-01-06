00f63d3d5f53da2852c2ee1d30749d29ab32135a
ttyfly <ttyfly@126.com>
2026-01-06 23:28:11 +0800

    Update: 初版 EntityManager

---
+++ Assets/Scripts/Entity/EntityBase.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public abstract class EntityBase : MonoBehaviour, IEntity
+{
+    //public abstract void Initialize(EntityContext context);
+
+    //public abstract void Tick(EntityContext context, float deltaSeconds);
+} 
